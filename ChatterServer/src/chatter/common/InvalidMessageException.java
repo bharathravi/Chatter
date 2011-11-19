3e3a96717ed9abc726c56285f3f6f47c745aa134
Bharath Ravi <bharathravi1@gmail.com>
2011-11-18 23:48:52 -0500

    Changing structure for easier builds

---
+++ ChatterServer/src/chatter/common/InvalidMessageException.java
@@ -0,0 +1,18 @@
+package chatter.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bharath
+ * Date: 11/12/11
+ * Time: 11:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InvalidMessageException extends Exception {
+  public InvalidMessageException(Exception e, String message) {
+    super(message,e);
+  }
+
+  public InvalidMessageException(String message) {
+    super(message);
+  }
+}
